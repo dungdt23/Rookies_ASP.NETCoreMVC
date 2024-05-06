19bc80a6883ffdb454d233d2a038ace05122ad38
drfred <dungtiendao01@gmail.com>
2024-05-07 00:53:45 +0700

    add areas

--- Rookies_ASP.NETCoreMVC.WebApp/Areas/NashTech/Controllers/RookiesController.cs
+++ Rookies_ASP.NETCoreMVC.WebApp/Areas/NashTech/Controllers/RookiesController.cs
@@ -1,24 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ClosedXML.Excel;
+using Microsoft.AspNetCore.Mvc;
 using Rookies_ASP.NETCoreMVC.BusinessLogic.BusinessLogic;
-using Rookies_ASP.NETCoreMVC.Models.Models;
 using Rookies_ASP.NETCoreMVC.Models.DTOs;
-using ClosedXML.Excel;
+using Rookies_ASP.NETCoreMVC.Models.Models;
 
-namespace Rookies_ASP.NETCoreMVC.WebApp.Controllers
+namespace Rookies_ASP.NETCoreMVC.WebApp.Areas.NashTech.Controllers
 {
+    [Area("NashTech")]
     public class RookiesController : Controller
     {
         private readonly IPersonBusinessLogic _personBusinessLogic;
-        public RookiesController(IPersonBusinessLogic personBusinessLogic)
+        private readonly ILogger<RookiesController> _logger;
+
+        public RookiesController(IPersonBusinessLogic personBusinessLogic, ILogger<RookiesController> logger)
         {
             _personBusinessLogic = personBusinessLogic;
+            _logger = logger;
         }
-        public IActionResult Index(int? birthYear)
+        public IActionResult Index(string? birthYear)
         {
             IEnumerable<Person> people = new List<Person>();
-            if (birthYear.HasValue)
+            if (!string.IsNullOrEmpty(birthYear))
             {
-                people = _personBusinessLogic.GetPeople(new FilterPersonDto { YearOfBirth = birthYear.Value });
+                try
+                {
+                    int parsedBirthYear = int.Parse(birthYear);
+                    people = _personBusinessLogic.GetPeople(new FilterPersonDto { YearOfBirth = parsedBirthYear });
+                }
+                catch (Exception ex)
+                {
+                    ViewData["errorMessage"] = "Invalid paramter";
+                    _logger.LogInformation(ex.Message);
+                }
             }
             else
             {
@@ -58,7 +71,7 @@ namespace Rookies_ASP.NETCoreMVC.WebApp.Controllers
             XLWorkbook wb = _personBusinessLogic.GetExcelFile();
             MemoryStream ms = new MemoryStream();
             wb.SaveAs(ms);
-            // Reset the stream position before returning
+            // Reset the stream position 
             ms.Position = 0;
             return File(ms.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "ListPeople.xlsx");
         }
--- Rookies_ASP.NETCoreMVC.WebApp/Program.cs
+++ Rookies_ASP.NETCoreMVC.WebApp/Program.cs
@@ -33,8 +33,14 @@ namespace Rookies_ASP.NETCoreMVC.WebApp
             app.UseAuthorization();
 
             app.MapControllerRoute(
+            name: "default",
+            pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+            app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
 
             app.Run();
         }
